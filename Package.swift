034daa66f4cbd7df7e3e84130a289c4b121d6ff6
pete-alphero <pete@alphero.com>
2024-03-15 14:58:30 +1300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.1.0")),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", range: "4.1.0"..."4.2.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", "4.1.0"..."4.2.0")),
     ],
     targets: [
         .target(
